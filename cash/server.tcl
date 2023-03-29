65bb78a143a3d353379e81eaa18292530b78f8eb
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2023-03-29 19:52:48 -0300

    TCL shortcut for Androwish

---
+++ cash/server.tcl
@@ -0,0 +1,3 @@
+#!/usr/bin/wish
+exec /storage/sdcard0/PortoDB/export/cash/ledger -f /storage/sdcard0/PortoDB/export/Journal.txt -q /storage/sdcard0/PortoDB/export/cash/quickview.txt -r /storage/sdcard0/PortoDB/export/cash/quickview.txt web
+exit 0
